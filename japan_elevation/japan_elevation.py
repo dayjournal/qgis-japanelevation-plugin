9600687e19bccad39474c26ca06d9aaf0d3b3613
Yasunori Kirimoto <yasunorikirimoto@gmail.com>
2026-01-17 18:26:40 +0900

    Add QGIS 4.0 (Qt6) compatibility

--- japan_elevation/japan_elevation.py
+++ japan_elevation/japan_elevation.py
@@ -1,8 +1,8 @@
 import os
 from typing import Callable, Optional
 
-from PyQt5.QtGui import QIcon
-from PyQt5.QtWidgets import QAction, QWidget
+from qgis.PyQt.QtGui import QIcon
+from qgis.PyQt.QtWidgets import QAction, QWidget
 
 from .ui.dialog.dialog import ElevationDialog
 from .utils.click_handler import ElevationClickHandler
